40d09845a37df0f1ec58007fef3664d21412cf36
aryan252006-creator <aryan25082006@gmail.com>
2025-12-18 16:54:24 +0530

    updated

---
+++ 03-props/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <div className="card">
+        <h2>Aryan</h2>
+      </div>
+      
+    </div>
+  )
+}
+
+export default App
